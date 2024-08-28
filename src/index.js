145aa23db6d3f42b37e4bc8d21bbe0a7e8454418
Etativel <farhanmaulana4611@gmail.com>
2024-08-28 20:15:58 +0700

    create basic container

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./styles/style.css"
+import favicon from "./images/icons/to-do-list.png"
+
+
+// Load icon to DOM
+const faviconContainer = document.querySelector(".favicon");
+faviconContainer.href = favicon;
