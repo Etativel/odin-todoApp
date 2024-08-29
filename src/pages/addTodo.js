ecc4ec8fba3b95fa329680a419a766069ad198bd
Etativel <farhanmaulana4611@gmail.com>
2024-08-29 15:38:27 +0700

    add todo to local storage

---
+++ src/pages/addTodo.js
@@ -0,0 +1,29 @@
+let storageKey = "todos";
+function addTodo(){
+    const taskName = document.querySelector('.task-name-input').value;
+    const taskDescription = document.querySelector('.task-desc-input').value;
+    const taskDueDate = document.querySelector('.due-date').value;
+    let taskPriority = document.querySelector('.task-priority').value;
+    const getId = new Date().toISOString();
+    if (!taskPriority) {
+        taskPriority = 'none';
+    };
+
+    const task = {
+            id: getId,
+            title: taskName,
+            description: taskDescription,
+            dueDate: taskDueDate,
+            priority: taskPriority,
+    };
+
+    const existingTasks = JSON.parse(localStorage.getItem(storageKey)) || [];
+
+    existingTasks.push(task);
+
+    localStorage.setItem(storageKey, JSON.stringify(existingTasks));
+    // localStorage.clear()
+    
+}
+
+    export default addTodo
