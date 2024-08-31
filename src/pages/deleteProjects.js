eced9b10643d06f91d8140ae168ec0c9180f0bbf
Etativel <farhanmaulana4611@gmail.com>
2024-08-31 17:13:00 +0700

    add delete feature for project

---
+++ src/pages/deleteProjects.js
@@ -0,0 +1,20 @@
+import loadTodo from "./loadTodo";
+import currentPage from "./currentPage";
+import loadProject from "./loadProject";
+
+const projectListStorage = 'projectList';
+
+function deleteProject(projectId){
+    // delete the project list
+    const getProjectList = JSON.parse(localStorage.getItem(projectListStorage));
+    const filterProject = getProjectList.filter(project => project.projectId !== projectId)
+    localStorage.setItem(projectListStorage, JSON.stringify(filterProject));
+
+    // delete the storage for that project
+    const todoKey = projectId;
+    localStorage.removeItem(todoKey)
+    loadProject()
+    loadTodo(currentPage.getCurrentPage())
+}
+
+export default deleteProject
