5308ab1e81e32c1daa5b69b472fd2292e00fa6da
Etativel <farhanmaulana4611@gmail.com>
2024-08-31 17:09:01 +0700

    add delete button eventlistener

--- src/pages/loadProject.js
+++ src/pages/loadProject.js
@@ -1,6 +1,6 @@
 import currentPage from "./currentPage";
 import loadTodo from "./loadTodo";
-
+import deleteProject from "./deleteProjects";
 const storageKey = 'projectList'
 function loadProject(){
 
@@ -13,8 +13,14 @@ function loadProject(){
         const container = document.createElement('div')
         container.classList.add('sub-project-btn')
         const projectDOM = `
-        <button class="project-btn active-btn active-project-btn" id=${project.projectId}>
-            ${project.projectName}
+        <button class="project-btn active-btn active-project-btn small-font" id=${project.projectId}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" style="width: 16px; height: 16px;">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5-3.9 19.5m-2.1-19.5-3.9 19.5" />
+        </svg>
+            <p>${project.projectName}<p>
+        <button class="delete-project-btn" id=${project.projectId}>
+            Del
+        </button>
         </button>`
         container.innerHTML = projectDOM;
         projectBtnContainer.appendChild(container);
@@ -27,7 +33,15 @@ function loadProject(){
             loadTodo(currentPage.getCurrentPage())
         });
     });
-    
+
+    const deleteBtn = document.querySelectorAll('.delete-project-btn')
+    deleteBtn.forEach(btn => {
+        btn.addEventListener('click', (e)=>{
+            deleteProject(e.target.id)
+        })
+    })
+
+        
 }
 
 export default loadProject
